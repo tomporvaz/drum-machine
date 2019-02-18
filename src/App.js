5a4dda4703611630f4dca03521d330f0ad60f505
Tom Porvaznik <tomporvaz@gmail.com>
2019-02-17 20:53:30 -0500

    Initial commit.  Created basic layout items.  Needs styling.  Ready for audio elements.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import bassDrum from './TR808_bass-drum.WAV';
 import './App.css';
 
 class App extends Component {
+
+  /* function needs this binding to work.  This function is only a test to understand how audio works.
+  playBassDrum () {
+    return bassDrum.play();
+  }
+  */
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Drum Machine</h1>
         </header>
+        <div id="drum-machine">
+          <div id="display">Test text</div>
+          <audio src={bassDrum} controls></audio>
+          <img src="./logo.svg"></img>
+          <div className="drum-pad" id="Q" >Q</div>
+          <div className="drum-pad" id="W">W</div>
+          <div className="drum-pad" id="E">E</div>
+
+          <div className="drum-pad" id="A">A</div>
+          <div className="drum-pad" id="S">S</div>
+          <div className="drum-pad" id="D">D</div>
+
+          <div className="drum-pad" id="Z">Z</div>
+          <div className="drum-pad" id="X">X</div>
+          <div className="drum-pad" id="C">C</div>
+        </div>
+
+
       </div>
     );
   }
