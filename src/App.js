531007214fb34bafa5610455ee6adb9e167ab6ed
Tom Porvaznik <tomporvaz@gmail.com>
2019-02-20 20:49:00 -0500

    imported all sounds, and started working on stateful execution of playing sound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import bassDrum from './TR808_bass-drum.WAV';
+import bassDrum from './TR808_bass-drum.mp3';
+import snare from './TR808_snare.mp3';
+import lowTomTom from './TR808_low-tom-tom.mp3';
+import lowConga from './TR808_low-conga.mp3';
+import midConga from './TR808_mid-conga.mp3';
+import highConga from './TR808_high-conga.mp3';
+import cowBell from './TR808_cow-bell.mp3';
+import clap from './TR808_clap.mp3';
+import closedHH from './TR808_closedHH.mp3';
+
 import './App.css';
 
 
@@ -8,18 +17,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //"input": sampleText,
+      "clicked": ""
       //"output": this.myMarkdown(sampleText)
     };
-    //this.playBassDrum = this.playBassDrum.bind(this);
+    this.playBassDrum = this.playBassDrum.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.playSound = this.playSound.bind(this);
 
   }
 
+
+
+  playSound (letter) {
+    let audio = document.getElementById("Q");
+    audio.play();
+  }
+
+  //use handle click to update state then us did update to play sound?
+
+  handleClick (letter) {
+    //this.setState({"clicked": letter});
+    this.playSound(letter);
+  }
   
 
   playBassDrum () {
     let audioQ = document.getElementById("Q");
     audioQ.play();
+  }
+
+  playCowBell () {
+    let audioW = document.getElementById("W");
+    audioW.play();
   }
 
 
@@ -34,8 +63,8 @@ class App extends Component {
           <div id="display">Test text</div>
           c
           
-          <div className="drum-pad" id="bassDrum" onClick={this.playBassDrum}>Q<audio src={bassDrum}id="Q"></audio></div>
-          <div className="drum-pad" id="W">W</div>
+          <div className="drum-pad" id="bassDrum" onClick={this.playSound}>Q<audio src={bassDrum} id="Q"></audio></div>
+          <div className="drum-pad" id="cowBell" onClick={this.playCowBell}>W<audio src={cowBell} id="W"></audio></div>
           <div className="drum-pad" id="E">E</div>
 
           <div className="drum-pad" id="A">A</div>
