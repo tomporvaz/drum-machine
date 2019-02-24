26e77d0bc2d126303851b01cb6d95b8fa57f718b
Tom Porvaznik <tomporvaz@gmail.com>
2019-02-23 21:26:52 -0500

    used arrow function to pass argument to handleclick and each button now uses the same handle click function with a different letter passed in

--- src/App.js
+++ src/App.js
@@ -28,17 +28,19 @@ class App extends Component {
 
 
 
-  playSound (letter) {
-    console.log(letter);
-    let audio = document.getElementById("Q");
-    audio.play();
-  }
+
 
   //use handle click to update state then us did update to play sound?
 
   handleClick (letter) {
     //this.setState({"clicked": letter});
     this.playSound(letter);
+    //console.log("State: " + this.state.clicked);
+  }  
+  playSound (letter) {
+    //console.log("play: " + letter);
+    let audio = document.getElementById(letter);
+    audio.play();
   }
   
 
@@ -65,17 +67,17 @@ class App extends Component {
           <div id="display">Test text</div>
           c
           
-          <div className="drum-pad" id="bassDrum" onClick={this.handleClick}>Q<audio src={bassDrum} id="Q"></audio></div>
-          <div className="drum-pad" id="cowBell" onClick={this.playCowBell}>W<audio src={cowBell} id="W"></audio></div>
-          <div className="drum-pad" id="E">E</div>
+          <div className="drum-pad" id="bassDrum" onClick={() => this.handleClick("Q")}>Q<audio src={bassDrum} id="Q"></audio></div>
+          <div className="drum-pad" id="lowTomTom" onClick={() => this.handleClick("W")}>W<audio src={lowTomTom} id="W"></audio></div>
+          <div className="drum-pad" id="snare" onClick={() => this.handleClick("E")}>E<audio src={snare} id="E"></audio></div>
 
-          <div className="drum-pad" id="A">A</div>
-          <div className="drum-pad" id="S">S</div>
-          <div className="drum-pad" id="D">D</div>
+          <div className="drum-pad" id="lowConga" onClick={() => this.handleClick("A")}>A<audio src={lowConga} id="A"></audio></div>
+          <div className="drum-pad" id="midConga" onClick={() => this.handleClick("S")}>S<audio src={midConga} id="S"></audio></div>
+          <div className="drum-pad" id="highConga" onClick={() => this.handleClick("D")}>D<audio src={highConga} id="D"></audio></div>
 
-          <div className="drum-pad" id="Z">Z</div>
-          <div className="drum-pad" id="X">X</div>
-          <div className="drum-pad" id="C">C</div>
+          <div className="drum-pad" id="cowbell" onClick={() => this.handleClick("Z")}>Z<audio src={cowBell} id="Z"></audio></div>
+          <div className="drum-pad" id="clap" onClick={() => this.handleClick("X")}>X<audio src={clap} id="X"></audio></div>
+          <div className="drum-pad" id="closedHH" onClick={() => this.handleClick("C")}>C<audio src={closedHH} id="C"></audio></div>
         </div>
 
 
