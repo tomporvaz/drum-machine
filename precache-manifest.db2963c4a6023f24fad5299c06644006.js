496b3bbdb5f2bd638182696f4834a30e7468d634
Tom Porvaznik <tomporvaz@gmail.com>
2019-03-03 19:54:02 -0500

    Updates

---
+++ precache-manifest.db2963c4a6023f24fad5299c06644006.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dcd4ffa02845fa2f4722f4ec91554b2",
+    "url": "/drum-machine/static/media/TR808_low-conga.3dcd4ffa.mp3"
+  },
+  {
+    "revision": "25b83916ea5205575765",
+    "url": "/drum-machine/static/css/main.93fcfd0b.chunk.css"
+  },
+  {
+    "revision": "c73e5ac2cc3e61c10a29",
+    "url": "/drum-machine/static/js/runtime~main.c73e5ac2.js"
+  },
+  {
+    "revision": "1f59217f449b83dfa68b",
+    "url": "/drum-machine/static/js/2.1f59217f.chunk.js"
+  },
+  {
+    "revision": "e9f52e6000dc1178de2f5f2a4817965a",
+    "url": "/drum-machine/static/media/TR808_bass-drum.e9f52e60.mp3"
+  },
+  {
+    "revision": "ae7a80c454b5c884c68e2a83a9a38c65",
+    "url": "/drum-machine/static/media/TR808_snare.ae7a80c4.mp3"
+  },
+  {
+    "revision": "b93c84b90f90ee33920647ce07578300",
+    "url": "/drum-machine/static/media/TR808_low-tom-tom.b93c84b9.mp3"
+  },
+  {
+    "revision": "25b83916ea5205575765",
+    "url": "/drum-machine/static/js/main.25b83916.chunk.js"
+  },
+  {
+    "revision": "87f2798d7c5c6c569eb395fb03e91d6a",
+    "url": "/drum-machine/static/media/TR808_mid-conga.87f2798d.mp3"
+  },
+  {
+    "revision": "751195d38ee17f77dc8dabba342d3593",
+    "url": "/drum-machine/static/media/TR808_high-conga.751195d3.mp3"
+  },
+  {
+    "revision": "62fe1b9dc6e6fd2b8182f781f3fc13ef",
+    "url": "/drum-machine/static/media/TR808_cow-bell.62fe1b9d.mp3"
+  },
+  {
+    "revision": "cbad116f2034cac4044a4e6656a41cff",
+    "url": "/drum-machine/static/media/TR808_clap.cbad116f.mp3"
+  },
+  {
+    "revision": "3b3923f0945846038f3861fa50ca009b",
+    "url": "/drum-machine/static/media/TR808_closedHH.3b3923f0.mp3"
+  },
+  {
+    "revision": "28c8feb738723c438eab880bfe9b04c4",
+    "url": "/drum-machine/index.html"
+  }
+];
